c394a19138620061db81df2cec9f0f8c667678a5
Зенин Сергей Андреевич (4016722) <szenin@dev.vtb>
2020-12-06 01:15:12 +0300

    feat: integrate with back

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
-
 const API_PATHS = {
   product: 'https://l8wsgw7aw5.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://qk5izicplh.execute-api.eu-west-1.amazonaws.com/dev/import',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://zeninzenin-nodejs-aws-cart-api-dev3.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
